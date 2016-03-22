5ed7615d2af8f1feef6d61a4f15a0a963299c622
s00144297 <s00144297@mail.itsligo.ie>
2016-03-22 22:51:01 +0000

    Styling, API and JQuery work

--- RAD302-S00144297/App_Start/BundleConfig.cs
+++ RAD302-S00144297/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace RAD302_S00144297
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.10.2.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -20,9 +20,20 @@ namespace RAD302_S00144297
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //JQuery UI
+            bundles.Add(new ScriptBundle("~/bundles/JQueryUI").Include(
+                        "~/Scripts/jquery-ui-1.11.4.min.js"));
+
+            //First Visit Popup
+            bundles.Add(new ScriptBundle("~/bundles/jqueryFirstVisitPopup").Include(
+            "~/Scripts/jquery.firstVisitPopup.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Dialog").Include(
+                     "~/Content/themes/base/dialog.css"));
         }
     }
 }
